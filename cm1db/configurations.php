d7d33de4f1c0f6cee825d0bd8865f0386bfbe7fc
l <wecanfxck@gmail.com>
2018-04-24 14:21:31 +0300

    make config file

---
+++ cm1db/configurations.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => '',
+        'dbname' => 'mortremas',
+    ],
+
+    'components' => [
+        'timezone' => [
+            'gtk' => 'ru',
+        ],
+
+        'interface' => [
+            'web' => 'hacker',
+            'web1' => 'guest',
+        ]
+    ]
+];
--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-    echo "MORTEMAS";
+         /////////
+        //  //
+       /////////
+      //  //
+     /////////
+    //  //
+   //////////
+  //  //
+ /////////     | система написана ---> SYNTAX <--- |
+
+
+
+include "header.php";
+
+echo "<h1> SYNTAX  </h1>";
+
+echo "<pre>";
+
+//принял конфиг
+$config = (file_get_contents('cm1db/configurations.php'));
+/////////////////////////////////////////////////////////////////
+print_r($config);
+echo '</pre>';
+
+
 ?>
